acdefa11f29c8b55e4dd0bf1e77e9f551bc5e224
Joseph Perez <hello@josephdanielperez.com>
2020-07-03 19:32:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -56,6 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages]
+		 
 end
 
 def adding_matz
@@ -81,6 +87,11 @@ def adding_matz
         }
      }
 
+programmer_hash => {:yukihiro_matsumoto => {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+		}
+	}
 
 end
 
@@ -101,7 +112,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
